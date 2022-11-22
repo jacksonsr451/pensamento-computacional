797cb4d1ec6f14e954ee7f10d78ec70bd38fa6ea
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-22 17:21:37 -0300

    include partials navigator

--- assets/js/include_pages.js
+++ assets/js/include_pages.js
@@ -1,13 +1,7 @@
-let pages_includes = document.getElementsByTagName('pages');
+$(() => {
+    $('.pages').load("./pages/default-content.html")   
+})
 
-for(var i=0; i<pages_includes.length; i++){
-    let include = pages_includes[i];
-    load_file(pages_includes[i].attributes.src.value, function(text){
-        include.insertAdjacentHTML('afterend', text);
-        include.remove();
-    });
-}
-
-function load_file(filename, callback) {
-    fetch(filename).then(response => response.text()).then(text => callback(text));
+function new_page(src) {
+    $('.pages').load(`./pages/${src}.html`);
 }
